d3b14fb423d1d4c27bf9c24bd5a28598cbec622d
Bruno-Pinto <bpintodasilva@gmail.com>
2025-05-10 18:24:30 +0200

    Added KeyListener to close on escape pressed

--- src/main/java/core/GameFrame.java
+++ src/main/java/core/GameFrame.java
@@ -1,17 +1,26 @@
 package core;
 
 import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GameFrame extends JFrame{
-	
-	GameFrame(GamePanel gamePanel){
 
+	GameFrame(Life life, GamePanel gamePanel){
 		add(gamePanel);
 		pack();
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setResizable(false);
 		setTitle("A Game of Life");
 		setLocationRelativeTo(null);
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+					life.stop();
+				}
+			}
+		});
 	}
 	
 }
