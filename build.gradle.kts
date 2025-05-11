be188c025910527450100e66608e0a85066a5cf6
Bruno-Pinto <bpintodasilva@gmail.com>
2025-05-11 02:45:16 +0200

    Updated gradle and idea settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 repositories {
@@ -23,6 +24,11 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+}
+
+javafx {
+    version = "23.0.1"
+    modules("javafx.controls", "javafx.fxml")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,4 +11,3 @@ plugins {
 }
 
 rootProject.name = "Mission-Life"
-include("src")
